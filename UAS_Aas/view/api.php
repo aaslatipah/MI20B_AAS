a574de3d728a99d3c41c549b56f1870ad89c68cc
AasLatipah <aaslatipah6@gmail.com>
2022-01-11 18:37:44 +0700

    Create api.php

---
+++ UAS_Aas/view/api.php
@@ -0,0 +1,30 @@
+<?php 
+    header("Access-Control-Allow-Origin:*");
+    header("Content-Type:application/json;charset=UTF-8");
+    header("Access-Control-Allow-Methods:POST,GET,PUT,DELETE");
+    header("Access-Control-Allow-Max-Age:3600");
+    header("Access-Control-Allow-Headers:Content-Type,Access-Control-Allow-Headers,Authorization,X-Requested-With");
+    //get Database Connection
+    include '../controller/Guru.php';
+    $ctrl = new Guru();
+    $request = $_SERVER['REQUEST_METHOD'];
+    switch($request)
+    {
+        case 'GET' :
+            $ctrl->getJenisData();
+            break;
+        case 'POST' :
+            $ctrl->setGolongan();
+        break;
+
+        case 'PUT' :
+        break;
+        case 'DELETE' :
+        break;
+        default;
+        http_response_code(404);
+        echo "Request tidak diizinkan";
+
+    }
+
+?>
